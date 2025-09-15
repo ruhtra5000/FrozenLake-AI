c13a91c2fd0e9eaa0a598d4ff4658e105f52a852
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-15 02:22:03 -0300

    refactor - shortening initialPopulation, and printing best individual per generation

--- code/GeneticManager.py
+++ code/GeneticManager.py
@@ -15,11 +15,7 @@ class GeneticManager:
 
     # Generates the first population (full random)
     def initialPopulation(self):
-        pop = []
-        for _ in range(self.populationLen):
-            pop.append(self.generateRandomChromosome())
-
-        return pop
+        return [self.generateRandomChromosome() for _ in range(self.populationLen)]
 
     # Generates a random chromosome
     def generateRandomChromosome(self):  
@@ -33,12 +29,14 @@ class GeneticManager:
     def tournament(self, env: gym.Env):
         bestIndividuals = []
         index = 0
+        bestFitness = 0.0
+        bestIndex = 0
         
         while index < self.populationLen:
             # Gets a group of individuals
             selectedIndividuals = self.population[index:(index+self.tournamentSize)]
             
-            # Seeks best individual
+            # Seeks best individual per group
             fitnessValues = [self.fitness(ind, env) for ind in selectedIndividuals]
 
             maxFitness = max(fitnessValues) 
@@ -46,9 +44,17 @@ class GeneticManager:
 
             bestIndividuals.append(self.population[(index+maxFitnessIndex)])
 
-            # Updates index (going to next group)
+            # Seeks best individual in general
+            if maxFitness > bestFitness:
+                bestFitness = maxFitness
+                bestIndex = index+maxFitnessIndex
+
+            # Updates index (going to the next group)
             index += self.tournamentSize
 
+        print("\n\nBest individual in generation:")
+        print(f"Fitness: {bestFitness} | Index: {bestIndex}")
+        print(self.population[bestIndex])
 
     # Fitness function
     def fitness(self, dna: list, env: gym.Env):
