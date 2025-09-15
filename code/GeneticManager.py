b5738eb81e58a2c0dc5d265cd8b9c3f28666276e
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-15 01:27:07 -0300

    feat - new atributes, fitness, and tournament at GeneticManager

--- code/GeneticManager.py
+++ code/GeneticManager.py
@@ -1,13 +1,71 @@
+import gymnasium as gym
+import random
 
 # Class responsible for all genetic procedures
 class GeneticManager:
     def __init__(self, actionSpace):
         self.actionSpace = actionSpace
+        self.populationLen = 150 # Number of individuals
+        self.population = self.initialPopulation()
+        self.tournamentSize = 3
+        self.episodesPerFitness = 30
+        self.maxStepsPerEpisode = 100
+        self.crossoverRate = 0.8
+        self.mutationRate = 0.05        
+
+    # Generates the first population (full random)
+    def initialPopulation(self):
+        pop = []
+        for _ in range(self.populationLen):
+            pop.append(self.generateRandomChromosome())
+
+        return pop
 
     # Generates a random chromosome
-    def generateChromosome(self):  
-        chromosome = ""
-        for _ in range(10):
-            chromosome += str(self.actionSpace.sample())
+    def generateRandomChromosome(self):  
+        chromosome = []
+        for _ in range(16):
+            chromosome.append(self.actionSpace.sample()) 
 
         return chromosome
+    
+    # Tournament selection
+    def tournament(self, env: gym.Env):
+        bestIndividuals = []
+        index = 0
+        
+        while index < self.populationLen:
+            # Gets a group of individuals
+            selectedIndividuals = self.population[index:(index+self.tournamentSize)]
+            
+            # Seeks best individual
+            fitnessValues = [self.fitness(ind, env) for ind in selectedIndividuals]
+
+            maxFitness = max(fitnessValues) 
+            maxFitnessIndex = fitnessValues.index(maxFitness)
+
+            bestIndividuals.append(self.population[(index+maxFitnessIndex)])
+
+            # Updates index (going to next group)
+            index += self.tournamentSize
+
+
+    # Fitness function
+    def fitness(self, dna: list, env: gym.Env):
+        totalReward = 0.0
+        for _ in range(self.episodesPerFitness):
+            state, _ = env.reset()
+            episodeReward = 0.0
+
+            for _ in range(self.maxStepsPerEpisode):
+                newState, reward, terminated, truncated, _ = env.step(dna[state])
+                episodeReward += reward
+                state = newState
+
+                if terminated or truncated:
+                    break
+
+            totalReward += episodeReward
+
+        #Returns average reward
+        return totalReward/self.maxStepsPerEpisode
