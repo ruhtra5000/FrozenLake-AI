965396b668bb9def67cc5a4afaed8c939fb4f30a
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-09-16 02:06:07 -0300

    feat - GA main loop, and avgFitness calculation

--- code/FrozenLake.py
+++ code/FrozenLake.py
@@ -1,17 +1,59 @@
 import gymnasium as gym
 from GeneticManager import GeneticManager
+from operator import itemgetter
 
 # Creating environment and some variables
-env = gym.make("FrozenLake-v1", render_mode = "human")
+env = gym.make("FrozenLake-v1", is_slippery=True, render_mode = None)
 
 geneticManager = GeneticManager(env.action_space)
 
-numberGenerations = 10
-stepsPerGeneration = 10 
+bestIndividual = {"ind": [], "fitness": 0.0}
+
+numberGenerations = 200
+
+# Calculates average fitness by generation
+def avgFitness(population):
+    totalFitness = 0.0
+    for ind in population:
+        totalFitness += ind["fitness"]
+    
+    return totalFitness/geneticManager.populationLen
 
 # "Main"
-for generation in range(numberGenerations):
-    env.reset()
-    dna = geneticManager.generateChromosome()
-    for step in range(stepsPerGeneration):
-        newState, reward, terminated, truncated, info = env.step(int(dna[step]))
+def main():
+    for generation in range(numberGenerations):
+        # Calculate fitness 
+        popWithFit = geneticManager.calculateFullPopulationFitness(env)
+        sortedPop = sorted(popWithFit, key=itemgetter('fitness'), reverse=True)
+        
+        # Get best individuals (elitism)
+        numIndividuals = int(geneticManager.populationLen / 10)
+        elite = sortedPop[0:numIndividuals]
+
+        # Prints (monitoring)
+        print(f"\nBest in gen {generation}:\n{elite[0]}")
+        print(f"Avg. Fitness: {avgFitness(popWithFit)}")
+
+        # Storaging best individual (overall)
+        if bestIndividual["fitness"] < elite[0]["fitness"]:
+            bestIndividual = elite[0]
+
+        # Generate new individuals
+        newPopulation = [ind["ind"] for ind in elite]
+        
+        while len(newPopulation) < geneticManager.populationLen:
+            parent1 = geneticManager.tournament(elite)["ind"]
+            parent2 = geneticManager.tournament(elite)["ind"]
+
+            child1, child2 = geneticManager.crossover(parent1, parent2)
+
+            child1 = geneticManager.mutation(child1)
+            child2 = geneticManager.mutation(child2)
+
+            newPopulation.append(child1)
+            newPopulation.append(child2)
+
+        geneticManager.population = newPopulation
+
+
+main()
